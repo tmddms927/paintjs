e5d8c93ab141a5669c2b2128d680e9ee3ad8270d
오승은 <tmddms927@gmail.com>
2022-02-09 17:58:50 +0900

    feat: 선 긋기 및 색상 선택 기능 (javascript)

--- app.js
+++ app.js
@@ -1,9 +1,47 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+
+canvas.width = 500;
+canvas.height = 500;
+
+ctx.strokeStyle = "#black";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function stopPainting() {
+	painting = false;
+}
+
+function startPainting() {
+	painting = true;
+}
 
 function onMouseMove(event) {
-	console.log(event);
+	const x = event.offsetX;
+	const y = event.offsetY;
+	if (!painting) {
+		ctx.beginPath();
+		ctx.moveTo(x, y);
+	} else {
+		ctx.lineTo(x, y);
+		ctx.stroke();
+	}
+}
+
+function handleColorClick(event) {
+	const color = event.target.style.backgroundColor;
+	console.log(color);
+	ctx.strokeStyle = color;
 }
 
 if (canvas) {
 	canvas.addEventListener("mousemove", onMouseMove);
+	canvas.addEventListener("mousedown", startPainting);
+	canvas.addEventListener("mouseup", stopPainting);
+	canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
+console.log(Array.from(colors));
